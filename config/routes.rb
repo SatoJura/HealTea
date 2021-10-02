e7adeba4c57dcf26dc9d5c1b207808f8a5261255
SatoJura <jnnsk0snb@gmail.com>
2021-10-02 01:33:02 +0000

    [Add]devise userのroutesにnamespace追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  
-  devise_for :users
-  
+  scope module: :public do
+    devise_for :users
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 end
