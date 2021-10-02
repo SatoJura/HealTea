62fd4bf339d2c531617cafc694e7b6d72c9e7b3f
SatoJura <jnnsk0snb@gmail.com>
2021-10-02 01:55:37 +0000

    [fix]devise userのroutes修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   scope module: :public do
-    devise_for :users
+    devise_for :users, controllers:{
+      sessions:      'public/users/sessions',
+      registrations: 'public/users/registrations'
+    }
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
 end
