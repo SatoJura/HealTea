b7952bdead824c8c6b30419ef7f241430639a091
SatoJura <jnnsk0snb@gmail.com>
2021-10-02 09:50:58 +0000

    [Add]userモデル　バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :nickname, length: { in: 2..10 },
+                       uniqueness: true 
+  
 end
