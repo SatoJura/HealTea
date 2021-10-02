64e80c4830d541a1343dd360d01297c757691b4d
SatoJura <jnnsk0snb@gmail.com>
2021-10-02 10:10:01 +0000

    [Add]herbモデル　バリデーション追加

--- app/models/herb.rb
+++ app/models/herb.rb
@@ -1,2 +1,7 @@
 class Herb < ApplicationRecord
+  attachment :herb_image
+  
+  varidates :herb_name, presence: true
+  varidates :department_name, presence: true
+  varidates :caption, presence: true
 end
