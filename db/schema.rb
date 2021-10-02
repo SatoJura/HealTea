515120eeffbc8e520c132cca2555931ddb81d88e
SatoJura <jnnsk0snb@gmail.com>
2021-10-02 10:14:29 +0000

    [Add]tagモデル・マイグレーションファイル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_02_095623) do
+ActiveRecord::Schema.define(version: 2021_10_02_101216) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 2021_10_02_095623) do
     t.string "herb_image_id"
     t.string "department_name"
     t.text "caption"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "tag_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
