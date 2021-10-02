2397a90fa5c283a02332bc962608c10104551b5c
SatoJura <jnnsk0snb@gmail.com>
2021-10-02 10:51:36 +0000

    [Add]post_commentモデル・マイグレーションファイル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_02_102642) do
+ActiveRecord::Schema.define(version: 2021_10_02_104758) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 2021_10_02_102642) do
     t.string "herb_image_id"
     t.string "department_name"
     t.text "caption"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "herb_id"
+    t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
