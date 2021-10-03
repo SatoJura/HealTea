72548507d18784c3595cb58f3a50a4e1b7a6d026
SatoJura <jnnsk0snb@gmail.com>
2021-10-03 02:06:33 +0000

    [Update]herbモデル　herb_nameのバリデーションをpresence→uniquenessに変更　＋　アソシエーション孫要素追 加

--- app/models/herb.rb
+++ app/models/herb.rb
@@ -1,11 +1,12 @@
 class Herb < ApplicationRecord
   has_many :post_comments
   has_many :likes
-  has_many :tag_relationships
-  
+  has_many :tag_relationships, dependent: :destroy
+    has_many  :tags, through: :tag_relationships
+
   attachment :herb_image
-  
-  validates :herb_name, presence: true
+
+  validates :herb_name, uniqueness: true
   validates :department_name, presence: true
   validates :caption, presence: true
 end
