e0e2db4a54224995cca041ca4a43fba16599163a
SatoJura <jnnsk0snb@gmail.com>
2021-10-07 14:27:53 +0000

    [Add]public herbサイドバー 効能タグに紐づいたハーブのみがリンクで飛ぶように変更

--- app/controllers/public/herbs_controller.rb
+++ app/controllers/public/herbs_controller.rb
@@ -1,15 +1,22 @@
 class Public::HerbsController < ApplicationController
   def index
-    @herbs = Herb.all
-    @tag_list = Tag.all
+      # サイドバーの効能タグから絞り込み
+    if params[:tag_id]
+      @tag_relationship = TagRelationship.where(tag_id: params[:tag_id]) # クリックした1つのタグに紐づく複数の情報を、中間テーブルから配列で取得
+      @herbs = @tag_relationship.pluck(:herb_id).map{ |x| Herb.find(x) } # 1つのタグに紐づくherb_id（複数）をmapで繰り返し処理し、取得
+      @tag_list = Tag.all
+    else
+      @herbs = Herb.all
+      @tag_list = Tag.all
+    end
   end
 
   def show
     @herb = Herb.find(params[:id])
     @tag_list = Tag.all
-    if params[:tag_id]
-      @tag = Tag.find(params[:tag_id])
-      @herbs = @tag.herbs
-    end
+    # if params[:tag_id]
+    #   @tag = Tag.find(params[:tag_id])
+    #   @herbs = @tag.herbs
+    # end
   end
 end
