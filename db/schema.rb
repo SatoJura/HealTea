accb15151e8f35b41128e3742e131bd90cdfaae2
SatoJura <jnnsk0snb@gmail.com>
2021-10-07 04:09:05 +0000

    [Add]tagsマイグレーションのtag_nameに null: falseを追加

--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 2021_10_02_105405) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string "tag_name"
+    t.string "tag_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
