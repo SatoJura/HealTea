ca6808434e74f83c6f88fc98c3bc38dd55f8d7d3
SatoJura <jnnsk0snb@gmail.com>
2021-10-07 08:01:04 +0000

    [Add]public サイドバー効能一覧

--- app/controllers/public/herbs_controller.rb
+++ app/controllers/public/herbs_controller.rb
@@ -4,5 +4,10 @@ class Public::HerbsController < ApplicationController
 
   def show
     @herb = Herb.find(params[:id])
+    @tag_list = Tag.all
+    if params[:tag_id]
+      @tag = Tag.find(params[:tag_id])
+      @herbs = @tag.herbs
+    end
   end
 end
