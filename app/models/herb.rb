3ade16c51aac89b5d795038a8a8002ec6bcc5b69
SatoJura <jnnsk0snb@gmail.com>
2021-10-13 06:05:38 +0000

    [Add]likeをfont-awesomeに変更

--- app/models/herb.rb
+++ app/models/herb.rb
@@ -13,16 +13,14 @@ class Herb < ApplicationRecord
   validates :caption, presence: true
 
   def save_tags(saveherb_tags)
-    current_tags = tags.pluck(:tag_name) unless tags.nil? # もともと保存されているすべてのタグ
-    old_tags = current_tags - saveherb_tags # 今回新たに保存したタグ以外の保存されたタグ
-    new_tags = saveherb_tags - current_tags # 今回保存したタグから、もともと保存されていたタグを除いた、新規のタグ
+    current_tags = tags.pluck(:tag_name) unless tags.nil?
+    old_tags = current_tags - saveherb_tags
+    new_tags = saveherb_tags - current_tags
 
-    # 保存するherbにとって不要なタグを外す（deleteする）
     old_tags.each do |old_name|
       tags.delete Tag.find_by(tag_name: old_name)
     end
 
-    # tagsにpost_tagを加える。（存在すればfind、新規ならcreateする）
     new_tags.each do |new_name|
       post_tag = Tag.find_or_create_by(tag_name: new_name)
       tags << post_tag
