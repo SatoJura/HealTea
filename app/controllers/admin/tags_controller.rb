60d9beb59135ac485691e72c003f93414fc09746
SatoJura <jnnsk0snb@gmail.com>
2021-10-15 02:30:41 +0000

    [Add]admin tag編集ページ作成

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -4,11 +4,26 @@ class Admin::TagsController < ApplicationController
   end
 
   def edit
+    @tag = Tag.find(params[:id])
   end
 
   def update
+    @tag = Tag.find(params[:id])
+    if @tag.update(tag_params)
+      flash[:success]="編集に成功しました"
+      redirect_to admin_tags_path(@tag)
+    else
+      flash.now[:alert]="編集に失敗しました"
+      render "edit"
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:tag_name)
   end
 end
